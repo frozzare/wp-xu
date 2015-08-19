85c5985f9e2ae0e0a565183c805419117d81fc5c
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-08-19 21:28:45 +0200

    Fix autoload for composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,41 @@
 <?php
 
-// Load Composer autoload.
+/**
+ * Load Composer autoload.
+ */
 require __DIR__ . '/../vendor/autoload.php';
 
-// Define fixtures directory constant
+/**
+ * Define fixtures directory constant
+ */
 define( 'XU_FIXTURE_DIR', __DIR__ . '/fixtures' );
 
-// Load xu file as plugin.
-WP_Test_Suite::load_plugins( __DIR__ . '/autoload.php' );
-
-// Load files.
+/**
+ * Load files.
+ */
 WP_Test_Suite::load_files( __DIR__ . '/class-unit-test-case.php' );
 
-// Run the WordPress test suite.
+/**
+ * Require files.
+ */
+require_once XU_FIXTURE_DIR . '/components/class-xu.php';
+
+/**
+ * Create a new foundation.
+ */
+$foundation = new \Xu\Foundation\Foundation;
+
+/**
+ * Boot the foundation.
+ */
+$foundation->boot();
+
+/**
+ * Register the xu test component.
+ */
+$foundation->register_component( 'xu', 'Xu\\Components\\Xu\\Xu' );
+
+/**
+ * Run the WordPress test suite.
+ */
 WP_Test_Suite::run();
