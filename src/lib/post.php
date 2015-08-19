72bab9e1639c6b5d2b9506034aa5794e924b520c
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-08-19 09:28:45 +0200

    Add xu_get_top_parent_post

--- src/lib/post.php
+++ src/lib/post.php
@@ -14,14 +14,31 @@ defined( 'ABSPATH' ) || exit;
  * @return bool
  */
 function xu_is_post_type( $id, $post_type = '' ) {
-	if ( ! is_numeric( $id ) ) {
-		$post_type = is_string( $id ) ? $id : '';
-		$id        = get_the_id();
-	}
+    if ( ! is_numeric( $id ) ) {
+        $post_type = is_string( $id ) ? $id : '';
+        $id        = get_the_id();
+    }
 
-	if ( ! is_string( $post_type ) ) {
-		throw new Exception( sprintf( '`%s` requires a valid post type.', __FUNCTION__ ) );
-	}
+    if ( ! is_string( $post_type ) ) {
+        throw new Exception( sprintf( '`%s` requires a valid post type.', __FUNCTION__ ) );
+    }
 
-	return $post_type === get_post_type( $id );
+    return $post_type === get_post_type( $id );
+}
+
+/**
+ * Get top-most parent post.
+ *
+ * @param  WP_Post $post
+ *
+ * @return WP_Post|array|null
+ */
+function xu_get_top_parent_post( WP_Post $post ) {
+    if ( $post->post_parent ) {
+        $ancestors = get_post_ancestors( $post->ID );
+        $root      = count( $ancestors ) - 1;
+        return get_post( $ancestors[$root] );
+    } else {
+        return $post;
+    }
 }
