d6d87c19449dd895bf3a02e017c61bb76dd591be
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-08-20 08:45:13 +0200

    Fix autoload paths

--- autoload.php
+++ autoload.php
@@ -3,11 +3,11 @@
 /**
  * Load Composer autoload if it exists.
  */
-if ( file_exists( __DIR__ . '/../vendor/autoload.php' ) ) {
-    require __DIR__ . '/../vendor/autoload.php';
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+    require __DIR__ . '/vendor/autoload.php';
 }
 
 /**
  * Register the WordPress autoload.
  */
-register_wp_autoload( 'Xu\\', __DIR__ . '/../src' );
+register_wp_autoload( 'Xu\\', __DIR__ . '/src' );
