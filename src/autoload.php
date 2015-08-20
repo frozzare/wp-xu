7322d8755e6b10f7d6816c1cba1e333a76674396
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-08-20 08:42:29 +0200

    Update autoload about code coverage ignore

--- src/autoload.php
+++ src/autoload.php
@@ -1,9 +1,9 @@
 <?php
 
+// @codeCoverageIgnoreStart
+
 /**
  * Load Composer autoload if it exists.
- *
- * @codeCoverageIgnore
  */
 if ( file_exists( __DIR__ . '/../vendor/autoload.php' ) ) {
     require __DIR__ . '/../vendor/autoload.php';
@@ -11,7 +11,7 @@ if ( file_exists( __DIR__ . '/../vendor/autoload.php' ) ) {
 
 /**
  * Register the WordPress autoload.
- *
- * @codeCoverageIgnore
  */
 register_wp_autoload( 'Xu\\', __DIR__ . '/../src' );
+
+// @codeCoverageIgnoreEnd
