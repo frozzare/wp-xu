e46b0efa6b3bff3f3cf6c245fbaf0a80f558a49a
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-07-25 20:52:56 +0200

    Add autoload file

---
+++ autoload.php
@@ -0,0 +1,15 @@
+<?php
+
+// Load Composer autoload if file exists.
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
+
+/**
+ * Hook xu into WordPress.
+ *
+ * @return \Xu\Foundation\Foundation
+ */
+xu_add_action( 'init', function () {
+	new \Xu\Foundation\Foundation;
+} );
