e1bfe577bffef7ebc56c4f632b4443867fbaa5ae
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2016-08-13 12:34:16 +0200

    Update tests

--- tests/cases/lib/utils-test.php
+++ tests/cases/lib/utils-test.php
@@ -2,9 +2,7 @@
 
 namespace Xu\Tests\Lib;
 
-use Xu\Tests\Unit_Test_Case;
-
-class Utils_Test extends Unit_Test_Case {
+class Utils_Test extends \WP_UnitTestCase {
 
 	public function test_xu_get_class_name() {
 		$this->assertSame( 'Say', xu_get_class_name( XU_FIXTURE_DIR . '/classes/class-say.php' ) );
